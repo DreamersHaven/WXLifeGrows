bfc380150bfde7a844b5090082ce178baf97d537
DreamersHaven <dongyaxin@dreamershaven.cn>
2020-03-16 22:04:02 +0800

    实现了暂存功能

--- packageWhatYouWant/pages/first/index.js
+++ packageWhatYouWant/pages/first/index.js
@@ -1,3 +1,5 @@
+
+
 Page({
   data: {
 
@@ -237,6 +239,11 @@ Page({
    */
   onLoad: function(options) {
     var me = this;
+
+    var collectioninfos = wx.getStorageSync("wantsInfo")
+    if (collectioninfos) {
+      this.data.wants= collectioninfos;
+    }
     me.setData({
       list: this.data.wants
     })
@@ -276,9 +283,26 @@ Page({
    * 页面中 【下一步】 按钮的点击事件
    */
   handleGoClick(e) {
+    //判断要执行什么操作
+    var buttonName=e.detail.target.id
+
+    //如果执行暂存操作，依据页面中数据更新数组内容，将数组保存到本地缓存中
+    if (buttonName=='saveBut'){
+      this.updateWants(e)
+      wx.setStorageSync("wantsInfo", this.data.wants)
+      wx.showToast({
+        title: '操作成功',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+
+
     if (this.data.current == 2) {
       return
     }
+
     //如果要进入第二步，将页面输入框控件的内容，绑定到数组
     //遍历form中表单所有控件信息，如果输入的需求项不足于35项，给予用户提示，是否进行下一步
     //将输入的需求，绑定到数组
@@ -287,60 +311,65 @@ Page({
 
       var that = this
 
-      this.data.wants[0].value = e.detail.value.alive1
-      this.data.wants[1].value = e.detail.value.alive2
-      this.data.wants[2].value = e.detail.value.alive3
-      this.data.wants[3].value = e.detail.value.alive4
-      this.data.wants[4].value = e.detail.value.alive5
-      this.data.wants[5].value = e.detail.value.alive6
-      this.data.wants[6].value = e.detail.value.alive7
+      this.updateWants(e)
 
-      this.data.wants[7].value = e.detail.value.love1
-      this.data.wants[8].value = e.detail.value.love2
-      this.data.wants[9].value = e.detail.value.love3
-      this.data.wants[10].value = e.detail.value.love4
-      this.data.wants[11].value = e.detail.value.love5
-      this.data.wants[12].value = e.detail.value.love6
-      this.data.wants[13].value = e.detail.value.love7
-
-      this.data.wants[14].value = e.detail.value.power1
-      this.data.wants[15].value = e.detail.value.power2
-      this.data.wants[16].value = e.detail.value.power3
-      this.data.wants[17].value = e.detail.value.power4
-      this.data.wants[18].value = e.detail.value.power5
-      this.data.wants[19].value = e.detail.value.power6
-      this.data.wants[20].value = e.detail.value.power7
-
-      this.data.wants[21].value = e.detail.value.free1
-      this.data.wants[22].value = e.detail.value.free2
-      this.data.wants[23].value = e.detail.value.free3
-      this.data.wants[24].value = e.detail.value.free4
-      this.data.wants[25].value = e.detail.value.free5
-      this.data.wants[26].value = e.detail.value.free6
-      this.data.wants[27].value = e.detail.value.free7
-
-      this.data.wants[28].value = e.detail.value.fun1
-      this.data.wants[29].value = e.detail.value.fun2
-      this.data.wants[30].value = e.detail.value.fun3
-      this.data.wants[31].value = e.detail.value.fun4
-      this.data.wants[32].value = e.detail.value.fun5
-      this.data.wants[33].value = e.detail.value.fun6
-      this.data.wants[34].value = e.detail.value.fun7
-
+      //对生成的需求数组进行如下处理
+      //1、如果用户输入的需求数不足35个，给予温馨提示，
+      //不强行限制，允许用户在输入不满35条的时,仍可以进行后续的操作
+      //2、随机打乱 需求顺序 并 去掉 value 为空的信息
 
       that.setData({
         list: this.data.wants
       })
-
-
-
     }
-
-
     const addCurrent = this.data.current + 1;
     this.handleClick(addCurrent)
   },
+  /**
+   * 内部使用函数
+   * 为需求数组重新赋值
+   */
+  updateWants(e){
+    this.data.wants[0].value = e.detail.value.alive1
+    this.data.wants[1].value = e.detail.value.alive2
+    this.data.wants[2].value = e.detail.value.alive3
+    this.data.wants[3].value = e.detail.value.alive4
+    this.data.wants[4].value = e.detail.value.alive5
+    this.data.wants[5].value = e.detail.value.alive6
+    this.data.wants[6].value = e.detail.value.alive7
 
+    this.data.wants[7].value = e.detail.value.love1
+    this.data.wants[8].value = e.detail.value.love2
+    this.data.wants[9].value = e.detail.value.love3
+    this.data.wants[10].value = e.detail.value.love4
+    this.data.wants[11].value = e.detail.value.love5
+    this.data.wants[12].value = e.detail.value.love6
+    this.data.wants[13].value = e.detail.value.love7
+
+    this.data.wants[14].value = e.detail.value.power1
+    this.data.wants[15].value = e.detail.value.power2
+    this.data.wants[16].value = e.detail.value.power3
+    this.data.wants[17].value = e.detail.value.power4
+    this.data.wants[18].value = e.detail.value.power5
+    this.data.wants[19].value = e.detail.value.power6
+    this.data.wants[20].value = e.detail.value.power7
+
+    this.data.wants[21].value = e.detail.value.free1
+    this.data.wants[22].value = e.detail.value.free2
+    this.data.wants[23].value = e.detail.value.free3
+    this.data.wants[24].value = e.detail.value.free4
+    this.data.wants[25].value = e.detail.value.free5
+    this.data.wants[26].value = e.detail.value.free6
+    this.data.wants[27].value = e.detail.value.free7
+
+    this.data.wants[28].value = e.detail.value.fun1
+    this.data.wants[29].value = e.detail.value.fun2
+    this.data.wants[30].value = e.detail.value.fun3
+    this.data.wants[31].value = e.detail.value.fun4
+    this.data.wants[32].value = e.detail.value.fun5
+    this.data.wants[33].value = e.detail.value.fun6
+    this.data.wants[34].value = e.detail.value.fun7
+  },
   /**
    * 页面中 【上一步】 按钮的点击事件
    */
@@ -387,5 +416,31 @@ Page({
       'isSecondShow': isSecondShow,
       'isThirdShow': isThirdShow
     })
+  },
+  /**
+   * 开关设置发生变化时，更改页面参数值
+   * 并将更给结果提交给后台服务器
+   */
+  onChange(event) {
+    var that = this
+    const detail = event.detail;
+    var keyValue = event.target.id
+    var list = that.data.wants
+    //依据系统开关的键值（开关英文名称），定位到List相关信息
+    var currentSwitch = ""
+    for (var i = 0, len = list.length; i < len; i++) {
+      if (keyValue == list[i].key) {
+        currentSwitch = "list[" + i + "].isChoose"
+        break
+      }
+    }
+
+    that.setData({
+      [currentSwitch]: detail.value
+    })
   }
+ 
+
+
+
 });
